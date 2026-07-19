0f48cf8954bbb004656d2e2d430b6e0a62f4abf5
Jason Marquette <1342427+jasonmarquette@users.noreply.github.com>
2026-07-18 20:33:08 -0500

    Support next and previous airport requests

--- src/c/app_message/app_message.c
+++ src/c/app_message/app_message.c
@@ -1,7 +1,6 @@
 #include "app_message.h"
 
 #include "../weather/weather.h"
-
 #include "../windows/main_window.h"
 
 static AppTimer *s_updated_timer = NULL;
@@ -17,82 +16,57 @@ static void inbox_received_handler(
 
   Tuple *airport_tuple =
       dict_find(iterator, MESSAGE_KEY_Airport);
-
   Tuple *category_tuple =
       dict_find(iterator, MESSAGE_KEY_Category);
-
   Tuple *temperature_tuple =
       dict_find(iterator, MESSAGE_KEY_TemperatureC);
-
   Tuple *pressure_tuple =
       dict_find(iterator, MESSAGE_KEY_PressureHpa);
-
   Tuple *wind_direction_tuple =
       dict_find(iterator, MESSAGE_KEY_WindDirection);
-
   Tuple *wind_speed_tuple =
       dict_find(iterator, MESSAGE_KEY_WindSpeedKt);
-
   Tuple *updated_tuple =
       dict_find(iterator, MESSAGE_KEY_UpdatedAt);
-
   Tuple *offline_tuple =
       dict_find(iterator, MESSAGE_KEY_Offline);
-
   Tuple *use_celsius_tuple =
       dict_find(iterator, MESSAGE_KEY_UseCelsius);
-
   Tuple *use_hpa_tuple =
       dict_find(iterator, MESSAGE_KEY_UseHpa);
 
   if (airport_tuple) {
-    weather_set_airport(
-        airport_tuple->value->cstring
-    );
+    weather_set_airport(airport_tuple->value->cstring);
   }
 
   if (category_tuple) {
-    weather_set_category(
-        category_tuple->value->cstring
-    );
+    weather_set_category(category_tuple->value->cstring);
   }
 
   if (temperature_tuple) {
-    weather_set_temperature_c(
-        temperature_tuple->value->int32
-    );
+    weather_set_temperature_c(temperature_tuple->value->int32);
   }
 
   if (pressure_tuple) {
-    weather_set_pressure_hpa(
-        pressure_tuple->value->int32
-    );
+    weather_set_pressure_hpa(pressure_tuple->value->int32);
   }
 
   if (wind_direction_tuple) {
-    weather_set_wind_direction(
-        wind_direction_tuple->value->int32
-    );
+    weather_set_wind_direction(wind_direction_tuple->value->int32);
   }
 
   if (wind_speed_tuple) {
-    weather_set_wind_speed_kt(
-        wind_speed_tuple->value->int32
-    );
+    weather_set_wind_speed_kt(wind_speed_tuple->value->int32);
   }
 
   if (updated_tuple) {
-  weather_set_updated_at(
-      updated_tuple->value->int32
-  );
-} else {
-  weather_set_updated_at(time(NULL));
-}
+    weather_set_updated_at(updated_tuple->value->int32);
+  } else {
+    weather_set_updated_at(time(NULL));
+  }
 
   if (offline_tuple) {
-    weather_set_offline(
-        offline_tuple->value->int32 != 0
-    );
+    weather_set_offline(offline_tuple->value->int32 != 0);
   } else {
     weather_set_offline(false);
   }
@@ -101,33 +75,27 @@ static void inbox_received_handler(
   bool use_hpa = false;
 
   if (use_celsius_tuple) {
-    use_celsius =
-        use_celsius_tuple->value->int32 != 0;
+    use_celsius = use_celsius_tuple->value->int32 != 0;
   }
 
   if (use_hpa_tuple) {
-    use_hpa =
-        use_hpa_tuple->value->int32 != 0;
+    use_hpa = use_hpa_tuple->value->int32 != 0;
   }
 
-  weather_set_units(
-      use_celsius,
-      use_hpa
-  );
-
+  weather_set_units(use_celsius, use_hpa);
   weather_refresh_display();
 
-main_window_set_updated("Updated now");
+  main_window_set_updated("Updated now");
 
-if (s_updated_timer) {
-  app_timer_cancel(s_updated_timer);
-}
+  if (s_updated_timer) {
+    app_timer_cancel(s_updated_timer);
+  }
 
-s_updated_timer = app_timer_register(
-    2000,
-    restore_updated_status,
-    NULL
-);
+  s_updated_timer = app_timer_register(
+      2000,
+      restore_updated_status,
+      NULL
+  );
 }
 
 static void inbox_dropped_handler(
@@ -161,9 +129,11 @@ static void outbox_failed_handler(
       "Outbox failed: %d",
       reason
   );
+
+  weather_refresh_status();
 }
 
-void app_message_request_weather(void) {
+static void send_request(uint32_t key, const char *description) {
   DictionaryIterator *iterator = NULL;
 
   AppMessageResult result =
@@ -172,52 +142,65 @@ void app_message_request_weather(void) {
   if (result != APP_MSG_OK || !iterator) {
     APP_LOG(
         APP_LOG_LEVEL_ERROR,
-        "Could not begin outbox: %d",
+        "Could not begin outbox for %s: %d",
+        description,
         result
     );
-
+    weather_refresh_status();
     return;
   }
 
-  dict_write_uint8(
-      iterator,
-      MESSAGE_KEY_RequestWeather,
-      1
-  );
-
+  dict_write_uint8(iterator, key, 1);
   result = app_message_outbox_send();
 
   if (result != APP_MSG_OK) {
     APP_LOG(
         APP_LOG_LEVEL_ERROR,
-        "Could not send request: %d",
+        "Could not send %s: %d",
+        description,
         result
     );
+    weather_refresh_status();
   }
+}
+
+void app_message_request_weather(void) {
+  send_request(
+      MESSAGE_KEY_RequestWeather,
+      "weather request"
+  );
+}
+
+void app_message_request_next_airport(void) {
+  send_request(
+      MESSAGE_KEY_RequestNextAirport,
+      "next airport request"
+  );
+}
+
+void app_message_request_previous_airport(void) {
+  send_request(
+      MESSAGE_KEY_RequestPreviousAirport,
+      "previous airport request"
+  );
 }
 
 void app_message_service_init(void) {
   app_message_register_inbox_received(
       inbox_received_handler
   );
-
   app_message_register_inbox_dropped(
       inbox_dropped_handler
   );
-
   app_message_register_outbox_sent(
       outbox_sent_handler
   );
-
   app_message_register_outbox_failed(
       outbox_failed_handler
   );
 
   AppMessageResult result =
-      app_message_open(
-          512,
-          128
-      );
+      app_message_open(512, 128);
 
   if (result != APP_MSG_OK) {
     APP_LOG(
@@ -229,5 +212,10 @@ void app_message_service_init(void) {
 }
 
 void app_message_service_deinit(void) {
+  if (s_updated_timer) {
+    app_timer_cancel(s_updated_timer);
+    s_updated_timer = NULL;
+  }
+
   app_message_deregister_callbacks();
 }
