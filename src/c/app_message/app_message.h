72eaed8c34bbbe1f962b16585253072afa9f37d1
Jason Marquette <1342427+jasonmarquette@users.noreply.github.com>
2026-07-18 20:32:40 -0500

    Add airport navigation requests

--- src/c/app_message/app_message.h
+++ src/c/app_message/app_message.h
@@ -5,3 +5,5 @@
 void app_message_service_init(void);
 void app_message_service_deinit(void);
 void app_message_request_weather(void);
+void app_message_request_next_airport(void);
+void app_message_request_previous_airport(void);
